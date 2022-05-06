7078a832cc0259952401dd63b243e1e0056cc852
chhotu marandi <cmarandi29@gmail.com>
2022-05-06 23:29:13 +0530

    for now javascript part is completed and project is ready,in the future may be I can add some features

---
+++ script.js
@@ -0,0 +1,15 @@
+const colors = ["red", "green", "blue", "rgba(0, 207, 255)", "#ffdd55"];
+const button = document.getElementById("btn");
+const color = document.querySelector(".color-value");
+
+btn.addEventListener("click", function() {
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
